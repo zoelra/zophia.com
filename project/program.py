3d795df007f707a513710c7da4912f0f2a55301f
Zophia Raleigh <zophiaraleigh1@gmail.com>
2019-01-16 23:36:51 -0600

    Updated files to fix bugs and build basic functionality

--- project/program.py
+++ project/program.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, session, flash, redirect, url_for, g
 from functools import wraps
+from hashlib import sha256
 import sqlite3
 
 DATABASE = 'blog.db'
-USERNAME = 'admin'
-PASSWORD = 'admin'
+USERNAME = 'fe7d80792aba17b08e301d3910bdef10e755086b2d03fa3ab99064a7ceff03cf'
+PASSWORD = '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918'
 SECRET_KEY = """b'|\xa9={5YR\x02{\x00\x97\xda\x16\x11L\x05\xbe\x1b\xde\xe9M\xbe\x8c\x89'"""
 
 app = Flask(__name__)
@@ -34,29 +35,41 @@ def landing_page():
     cur = g.db.execute('select * from posts')
     posts = [dict(title=row[0], post=row[1]) for row in cur.fetchall()]
     g.db.close()
-    return render_template('main.html', posts=posts)
+    return render_template('visitor.html', posts=posts)
 
 
 @app.route('/admin', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
+        if sha256(request.form['username'].encode('utf-8')).hexdigest()\
+                != app.config['USERNAME']\
+                or sha256(request.form['password'].encode('utf-8')).hexdigest()\
+                != app.config['PASSWORD']:
             error = 'Invalid Credentials. Please try again.'
         else:
             session['logged_in'] = True
-            return redirect(url_for('main'))
+            return redirect(url_for('backend'))
     return render_template('login.html', error=error)
 
 
 @app.route('/main')
-@login_required
 def main():
     g.db = connect_db()
     cur = g.db.execute('select * from posts')
     posts = [dict(title=row[0], post=row[1]) for row in cur.fetchall()]
     g.db.close()
-    return render_template('main.html', posts=posts)
+    return render_template('visitor.html', posts=posts)
+
+
+@app.route('/backend')
+@login_required
+def backend():
+    g.db = connect_db()
+    cur = g.db.execute('select * from posts')
+    posts = [dict(title=row[0], post=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('adding_posts.html', posts=posts)
 
 
 @app.route('/add', methods=['POST'])
@@ -66,14 +79,14 @@ def add():
     post = request.form['post']
     if not title or not post:
         flash("All fields are required. Please try again")
-        return redirect(url_for('main'))
+        return redirect(url_for('backend'))
     else:
         g.db = connect_db()
         g.db.execute('insert into posts (title, post) values (?, ?)', [request.form['title'], request.form['post']])
         g.db.commit()
         g.db.close()
         flash('New entry was successfully posted!')
-        return redirect(url_for('main'))
+        return redirect(url_for('backend'))
 
 
 @app.route('/logout')
